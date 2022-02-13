1f6f5af6a363a3831773c0b72d38b3b5ef46c5a6
Paul Munyao <paulmunyao094@gmail.com>
2022-02-13 13:03:42 +0300

    Defining class user in user.ts

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,10 @@
 export class User {
+  constructor(
+    public profile: UserProfile,
+    public name: string,
+    public location: string,
+    public email: string,
+    public followers: number,
+    public following: number
+  ) {}
 }
