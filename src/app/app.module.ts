a1c06ea67d4268efbf18025c45eac577dfb08968
Paul Munyao <paulmunyao094@gmail.com>
2022-02-13 16:33:47 +0300

    Add get user function into component.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { User } from '../user';
+import { FormServiceService } from '../form-service/.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
+  providers: [FormServiceService],
 })
 export class FormComponent implements OnInit {
   user!: User;
+  getUser(){
+    return User
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -19,9 +24,16 @@ export class FormComponent implements OnInit {
       followers: number;
       following: number;
     }
-    this.http.get<ApiResponse>("https://api.github.com/user").subscribe(data=>{
-      this.user = new User(data.name,data.location,data.email,data.followers,data.following)
-
-    })
+    this.http
+      .get<ApiResponse>('https://api.github.com/user')
+      .subscribe((data) => {
+        this.user = new User(
+          data.name,
+          data.location,
+          data.email,
+          data.followers,
+          data.following
+        );
+      });
   }
 }
