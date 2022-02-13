7cc206cb6aab4a1ebbfffb27c51365e2f14aa90d
Paul Munyao <paulmunyao094@gmail.com>
2022-02-13 16:45:10 +0300

    Include http inside the constructor

--- src/app/form-service.service.ts
+++ src/app/form-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { observable } from 'rxjs'
 @Injectable({
   providedIn: 'root'
@@ -10,5 +10,5 @@ export class FormServiceService {
     return User
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
