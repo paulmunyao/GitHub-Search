e5d8e6480417a945425e50c6e9e782dd6473387b
Paul Munyao <paulmunyao094@gmail.com>
2022-02-13 16:57:36 +0300

    Import service from form service servive folder

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { User } from '../user';
-// import { FormServiceService } from '../form-service/.service';
+import { FormServiceService } from '../form-service.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
-  // providers: [FormServiceService],
+  providers: [FormServiceService],
 })
 export class FormComponent implements OnInit {
   user!: User;
