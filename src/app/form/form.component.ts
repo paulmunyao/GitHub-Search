949fd5645378ce007fc3e1b56d9186972d7088a2
Paul Munyao <paulmunyao094@gmail.com>
2022-02-13 13:37:50 +0300

    Assigning class user to User inside the component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,6 +7,7 @@ import { User } from '../user';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  user!: User;
 
   constructor(private http: HttpClient) { }
 
