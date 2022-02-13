22deeec86ce1c6d9e9fba29d11156c8af8db7f64
Paul Munyao <paulmunyao094@gmail.com>
2022-02-13 18:07:10 +0300

    Register form service as a provider in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormServiceService } from './form-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
- 
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+
+  providers: [FormServiceService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
